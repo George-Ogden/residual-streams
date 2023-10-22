b5aea3e2c2b21f31fdb8749398aacdba68dee366
George-Ogden <george-ogden@outlook.com>
2023-10-22 09:49:53 +0100

    update metric for non-uniform residual sizes

--- src/metrics/distribution.py
+++ src/metrics/distribution.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 
-from typing import Dict
+from typing import Dict, List
 
 from .base import Metric, VariableLengthClassifierOutput
 
@@ -11,9 +11,9 @@ class Distribution(Metric):
     def update(self, batch: Dict[str, torch.Tensor], model_output: VariableLengthClassifierOutput):
         # L x [B, N, H]
         self.value += np.array([
-            self.process(activations.cpu()).mean(axis=-2).sum(-2)
+            self.process(activations.cpu()).mean(axis=-2).sum(-2).numpy()
             for activations in model_output.layer_activations
-        ])
+            ], dtype=object)
         super().update(batch, model_output)
     
     @classmethod
@@ -21,3 +21,9 @@ class Distribution(Metric):
         sorted_values = torch.sort(activations, dim=-1).values
         normalised_values = sorted_values / torch.abs(sorted_values).sum(dim=-1, keepdim=True)
         return normalised_values
+
+    def compute(self) -> List[List[float]]:
+        results = super().compute()
+        return np.array([
+            result.tolist() for result in results
+        ], dtype=object)
